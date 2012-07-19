e6e9e430299968268893ba53b6895668f109525d
larapid <pingvishtein@gmail.com>
2012-07-19 16:34:37 +0400

    added site definition project

--- create-packages.bat
+++ create-packages.bat
@@ -1,4 +1,5 @@
 set msbuild="C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 set config=Release
-set outdir=%CD%\Packages\bin
+set outdir=%CD%\Packages\
 %msbuild% /p:Configuration=%config% "FLS.SharePoint/FLS.SharePoint.csproj" /t:Package /p:BasePackagePath=%outdir%
+%msbuild% /p:Configuration=%config% "FLS.SharePoint.SiteStructure/FLS.SharePoint.SiteStructure.csproj" /t:Package /p:BasePackagePath=%outdir%
