9de14a12667c398c3ca2156250548862da683008
larapid <pingvishtein@gmail.com>
2012-07-24 15:24:36 +0400

    added utils project

---
+++ Source/FLS.SharePoint.Utils/ConfigPropertiesParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FLS.SharePoint.Utils
+{
+    public class ConfigPropertiesParser : IConfigPropertiesParser
+    {
+        private const char Separator = ';';
+        
+        public uint ToUInt(string propertyValue)
+        {
+            return uint.Parse(propertyValue);
+        }
+
+        public string[] ToStringArray(string propertyValue)
+        {
+            return propertyValue.Split(Separator);
+        }
+
+        public Uri ToUri(string propertyValue)
+        {
+            return new Uri(propertyValue);
+        }
+    }
+}
---
+++ Source/FLS.SharePoint.Utils/IConfigPropertiesParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FLS.SharePoint.Utils
+{
+    public interface IConfigPropertiesParser
+    {
+        uint ToUInt(string propertyValue);
+
+        string[] ToStringArray(string propertyValue);
+
+        Uri ToUri(string propertyValue);
+    }
+}
