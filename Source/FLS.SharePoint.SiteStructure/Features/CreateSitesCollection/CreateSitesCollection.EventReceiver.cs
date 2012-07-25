b562ba9399de32b89ca26c046d5f25402359c841
larapid <pingvishtein@gmail.com>
2012-07-25 15:21:19 +0400

    fixed created site feature

--- Source/FLS.SharePoint.SiteStructure/Features/CreateSitesCollection/CreateSitesCollection.EventReceiver.cs
+++ Source/FLS.SharePoint.SiteStructure/Features/CreateSitesCollection/CreateSitesCollection.EventReceiver.cs
@@ -2,7 +2,6 @@ using System.IO;
 using System.Runtime.InteropServices;
 using FLS.SharePoint.Infrastructure;
 using FLS.SharePoint.Utils;
-using Microsoft.Practices.ServiceLocation;
 using Microsoft.Practices.SharePoint.Common.Logging;
 using Microsoft.Practices.SharePoint.Common.ServiceLocation;
 using Microsoft.SharePoint;
@@ -12,19 +11,12 @@ namespace FLS.SharePoint.SiteStructure.Features.CreateSitesCollection
     [Guid("68f9b364-9421-4b00-8408-6908a439bacd")]
     public class CreateSitesCollectionEventReceiver : SPFeatureReceiver
     {
-        private readonly IConfigPropertiesParser configPropertiesParser;
-        private readonly IServiceLocator serviceLocator;
-        private readonly ILogger log;
-        
-        public CreateSitesCollectionEventReceiver()
-        {
-            serviceLocator = SharePointServiceLocator.GetCurrent();
-            configPropertiesParser = serviceLocator.GetInstance<IConfigPropertiesParser>();
-            log = serviceLocator.GetInstance<ILogger>();
-        }
-
         public override void FeatureActivated(SPFeatureReceiverProperties properties)
         {
+            var serviceLocator = SharePointServiceLocator.GetCurrent();
+            var configPropertiesParser = serviceLocator.GetInstance<IConfigPropertiesParser>();
+            var log = serviceLocator.GetInstance<ILogger>();
+
             log.Debug("Starting activation of a feature");
             var webs = GetAvailableWebs(properties);
             var siteNames = GetSiteNames(properties);
@@ -61,7 +53,7 @@ namespace FLS.SharePoint.SiteStructure.Features.CreateSitesCollection
 
         private static SPWebCollection GetAvailableWebs(SPFeatureReceiverProperties properties)
         {
-            return ((SPWeb) properties.Feature.Parent).Site.AllWebs;
+            return ((SPWeb)properties.Feature.Parent).Site.AllWebs;
         }
 
         private static void SafelyRemoveSubSite(string siteName, SPWebCollection webs)
@@ -79,6 +71,8 @@ namespace FLS.SharePoint.SiteStructure.Features.CreateSitesCollection
 
         private string[] GetSiteNames(SPFeatureReceiverProperties properties)
         {
+            var serviceLocator = SharePointServiceLocator.GetCurrent();
+            var configPropertiesParser = serviceLocator.GetInstance<IConfigPropertiesParser>();
             return configPropertiesParser.ToStringArray(properties.Feature.Properties["SiteNames"].Value);
         }
     }
