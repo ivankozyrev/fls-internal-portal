ba7ab528f8a2e13fc6ca023108fd8b6115d0bef2
larapid <pingvishtein@gmail.com>
2012-07-26 11:46:19 +0400

    added test feature, that should be deleted later

---
+++ Source/FLS.SharePoint.SiteStructure/Features/AddTestSite/AddTestSite.EventReceiver.cs
@@ -0,0 +1,60 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+using Microsoft.SharePoint.Security;
+
+namespace FLS.SharePoint.SiteStructure.Features.AddTestSite
+{
+    /// <summary>
+    /// This class handles events raised during feature activation, deactivation, installation, uninstallation, and upgrade.
+    /// </summary>
+    /// <remarks>
+    /// The GUID attached to this class may be used during packaging and should not be modified.
+    /// </remarks>
+
+    [Guid("a50c7be2-c9d1-417f-9021-45cb210d6af4")]
+    public class AddTestSiteEventReceiver : SPFeatureReceiver
+    {
+        // Uncomment the method below to handle the event raised after a feature has been activated.
+
+        public override void FeatureActivated(SPFeatureReceiverProperties properties)
+        {
+            var currentSiteCollection = (SPSite)properties.Feature.Parent;
+            SPWebApplication webApp = currentSiteCollection.WebApplication;
+            var language = (uint)webApp.Sites[0].AllWebs[0].Locale.LCID;
+            SPSite site = webApp.Sites.Add("/sites/simpleSite", "SharePoint", null, language, "STS#1", "localadmin",
+                                           "Administrator", "admin@contoso.com");
+        }
+
+
+        // Uncomment the method below to handle the event raised before a feature is deactivated.
+
+        //public override void FeatureDeactivating(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+
+        // Uncomment the method below to handle the event raised after a feature has been installed.
+
+        //public override void FeatureInstalled(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+
+        // Uncomment the method below to handle the event raised before a feature is uninstalled.
+
+        //public override void FeatureUninstalling(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+        // Uncomment the method below to handle the event raised when a feature is upgrading.
+
+        //public override void FeatureUpgrading(SPFeatureReceiverProperties properties, string upgradeActionName, System.Collections.Generic.IDictionary<string, string> parameters)
+        //{
+        //}
+    }
+}
