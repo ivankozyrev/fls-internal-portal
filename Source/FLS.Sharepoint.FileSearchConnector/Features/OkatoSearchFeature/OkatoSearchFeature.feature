d8ee2d05003629b6e9d5ba396907c550a5644b0c
Alexandr Sorokin <alexander.s.sorokin@firstlinesoftware.com>
2012-07-26 17:18:49 +0400

    Created file search connector

---
+++ Source/FLS.Sharepoint.FileSearchConnector/Features/OkatoSearchFeature/OkatoSearchFeature.feature
@@ -0,0 +1,2 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="d9e44245-1c7d-4f16-9dae-6a75aef3f23a" featureId="d9e44245-1c7d-4f16-9dae-6a75aef3f23a" imageUrl="" scope="Farm" solutionId="00000000-0000-0000-0000-000000000000" title="Поиск по справочнику ОКАТО" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel" />
---
+++ Source/FLS.Sharepoint.FileSearchConnector/OkatoModel/OkatoService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FLS.Sharepoint.FileSearchConnector.OkatoModel
+{
+    public class OkatoService
+    {
+        public static OkatoEntity ReadItem(string id)
+        {
+            return ReadList().SingleOrDefault(c => c.Id.ToUpper().Equals(id.ToUpper()));
+        }
+
+        public static IEnumerable<OkatoEntity> ReadList()
+        {
+            var entityList = new List<OkatoEntity>();
+            using (var reader = new StreamReader(@"c:\okato.txt", Encoding.GetEncoding(1251)))
+            {
+                string row;
+                while ((row = reader.ReadLine()) != null)
+                {
+                    var readerData = row.Split("|".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                    var entity = new OkatoEntity
+                                     {
+                                         Id = readerData[0],
+                                         City = readerData.Length > 2 ? readerData[2] : readerData[1]
+                                     };
+                    entityList.Add(entity);
+                }
+            }
+
+            return entityList;
+        }
+    }
+} 
