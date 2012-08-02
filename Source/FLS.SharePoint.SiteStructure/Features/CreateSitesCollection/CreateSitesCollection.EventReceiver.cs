d06eb2281463e92db9c9f4a93448e10cea745e62
Evgeny Skurikhin <eskurihin@gmail.com>
2012-08-02 12:32:25 +0400

    applying custom web templates during sites creation

--- Source/FLS.SharePoint.SiteStructure/Features/CreateSitesCollection/CreateSitesCollection.EventReceiver.cs
+++ Source/FLS.SharePoint.SiteStructure/Features/CreateSitesCollection/CreateSitesCollection.EventReceiver.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Runtime.InteropServices;
 using FLS.SharePoint.Infrastructure;
@@ -14,31 +15,44 @@ namespace FLS.SharePoint.SiteStructure.Features.CreateSitesCollection
     [Guid("68f9b364-9421-4b00-8408-6908a439bacd")]
     public class CreateSitesCollectionEventReceiver : SPFeatureReceiver
     {
+        private readonly IServiceLocator serviceLocator;
+
+        private readonly IConfigPropertiesParser configPropertiesParser;
+
+        private readonly IFileSystemHelper fileSystemHelper;
+
+        private readonly ILogger logger;
+
+        public CreateSitesCollectionEventReceiver()
+        {
+            serviceLocator = SharePointServiceLocator.GetCurrent();
+            configPropertiesParser = serviceLocator.GetInstance<IConfigPropertiesParser>();
+            fileSystemHelper = serviceLocator.GetInstance<IFileSystemHelper>();
+            logger = serviceLocator.GetInstance<ILogger>();
+        }
+
         public override void FeatureActivated(SPFeatureReceiverProperties properties)
         {
-            IServiceLocator serviceLocator =
-                SharePointServiceLocator.GetCurrent();
-            IConfigPropertiesParser configPropertiesParser =
-                serviceLocator.GetInstance<IConfigPropertiesParser>();
-            ILogger log = serviceLocator.GetInstance<ILogger>();
-
-            log.Debug("start activating feature");
+            logger.Debug(string.Format("Start feature {0} activation.", properties.Feature.Definition.DisplayName));
+            logger.Debug("Reading configuration...");
             var configuration = GetSitesConfiguration(properties,
                                                       configPropertiesParser);
+            logger.Debug("Done. Reading configuration.");
             var rootSite = GetRootSite(properties);
-            var siteCollection = GetSiteCollection(properties);
-            var rootSiteWeb = GetSiteRootWeb(rootSite);
+            var sites = GetSiteCollections(properties);
+            var rootWeb = GetSiteRootWeb(rootSite);
 
+            logger.Debug("Creating groups...");
             foreach (var group in configuration.Groups)
             {
-                if (!ContainsGroup(rootSiteWeb.SiteGroups, group.Name))
+                if (!ContainsGroup(rootWeb.SiteGroups, group.Name))
                 {
-                    var owner = rootSiteWeb.EnsureUser(configuration.SiteOwner);
-                    rootSiteWeb.SiteGroups.Add(group.Name, owner, owner,
+                    var owner = rootWeb.EnsureUser(configuration.SitesOwner);
+                    rootWeb.SiteGroups.Add(group.Name, owner, owner,
                                            group.Description);
                 }
 
-                var newGroup = rootSiteWeb.SiteGroups[group.Name];
+                var newGroup = rootWeb.SiteGroups[group.Name];
                 foreach (var user in group.Users)
                 {
                     newGroup.AddUser(user.Login, string.Empty, user.Login,
@@ -46,64 +60,135 @@ namespace FLS.SharePoint.SiteStructure.Features.CreateSitesCollection
                 }
             }
 
+            logger.Debug("Done. Creation of groups");
             foreach (var site in configuration.Sites)
             {
-                if (SiteCollectionExists(siteCollection, site.Name))
+                logger.Debug(string.Format("creating site: {0}", site.Name));
+                if (SiteCollectionExists(sites, site.Name))
                 {
-                    siteCollection.Delete(site.Name);
+                    logger.Debug(string.Format("Site {0} already exists. Attempting to delete it.", site.Name));
+                    sites.Delete(site.Name);
+                    logger.Debug(string.Format("Done. Site {0} deletion.", site.Name));
                 }
 
-                var newSiteCollection = siteCollection.Add(
+                logger.Debug(string.Format("Creating new empty site: {0}", site.Name));
+                var newSite = sites.Add(
                     site.Name,
                     site.Title,
                     site.Description,
-                    (uint)GetSiteRootWeb(rootSite).Locale.LCID,
-                    site.WebTemplate,
-                    configuration.SiteOwner,
-                    configuration.SiteOwner,
+                    GetLocale(rootWeb),
+                    null,
+                    configuration.SitesOwner,
+                    configuration.SitesOwner,
                     string.Empty);
+                logger.Debug(string.Format("Done. Empty site {0} creation.", site.Name));
+                
+                logger.Debug(
+                    string.Format(
+                        "Loading template: {0} from package: {1} in folder: {2}", 
+                        site.WebTemplate.Name, 
+                        site.WebTemplate.PackageFileName, 
+                        configuration.TemplatesDirectory));
+                LoadTemplateFromPackage(
+                    newSite, 
+                    site.WebTemplate.PackageFileName, 
+                    configuration.TemplatesDirectory);
+                logger.Debug(string.Format("Done. Loading template: {0}", site.WebTemplate.Name));
 
+                logger.Debug(string.Format(
+                    "Applying template: {0} to empty site: {1}", 
+                    site.WebTemplate.Name, 
+                    site.Name));
+                ApplyWebTemplate(GetSiteRootWeb(newSite), site.WebTemplate.Name);
+                logger.Debug(string.Format(
+                    "Done. Applying template: {0} to site: {1}", 
+                    site.WebTemplate.Name, 
+                    site.Name));
+
+                logger.Debug(string.Format("Granting permissions for site {0}", site.Name));
                 foreach (var permission in site.Permissions)
                 {
-                    var roleAssignment =
-                        new SPRoleAssignment(
-                            rootSiteWeb.SiteGroups[permission.GroupName]);
-                    roleAssignment.RoleDefinitionBindings.Add(
-                        rootSiteWeb.RoleDefinitions.GetById(permission.PermissionLevelId));
-                    GetSiteRootWeb(newSiteCollection).RoleAssignments.Add(roleAssignment);
+                    logger.Debug(string.Format(
+                        "Granting permissionLevel: {0} to group: {1} on site {2}", 
+                        permission.PermissionLevelId, 
+                        permission.GroupName, 
+                        site.Name));
+                    var roleAssignment = new SPRoleAssignment(rootWeb.SiteGroups[permission.GroupName]);
+                    roleAssignment.RoleDefinitionBindings.Add(rootWeb.RoleDefinitions.GetById(permission.PermissionLevelId));
+                    GetSiteRootWeb(newSite).RoleAssignments.Add(roleAssignment);
+                    logger.Debug(string.Format(
+                        "Done. Granting permissionLevel: {0} to group: {1} on site {2}", 
+                        permission.PermissionLevelId, 
+                        permission.GroupName, 
+                        site.Name));
                 }
+                logger.Debug(string.Format("Done. Granting permissions for site {0}", site.Name));
             }
+
+            logger.Debug(string.Format("Done. Feature {0} activation.", properties.Feature.Definition.DisplayName));
         }
 
         public override void FeatureDeactivating(SPFeatureReceiverProperties properties)
         {
-            IServiceLocator serviceLocator = SharePointServiceLocator.GetCurrent();
-            IConfigPropertiesParser configPropertiesParser = serviceLocator.GetInstance<IConfigPropertiesParser>();
-            
+            logger.Debug(string.Format("Start feature {0} deactivation", properties.Feature.Definition.DisplayName));
+            logger.Debug("Reading configuration...");
             var configuration = GetSitesConfiguration(properties, configPropertiesParser);
-            var sites = GetSiteCollection(properties);
+            logger.Debug("Done. Reading configuration");
+
+            var sites = GetSiteCollections(properties);
             var rootSite = GetRootSite(properties);
-            foreach (var collection in configuration.Sites)
+
+            logger.Debug("Start deleting sites...");
+            foreach (var site in configuration.Sites)
             {
-                if (SiteCollectionExists(sites, collection.Name))
+                logger.Debug(string.Format("Processing site: {0}", site.Name));
+                if (SiteCollectionExists(sites, site.Name))
                 {
-                    var site = sites[collection.Name];
-                    foreach (var permission in collection.Permissions)
+                    logger.Debug(string.Format("Site: {0} has been found. Deleting permissions.", site.Name));
+                    var siteToDelete = sites[site.Name];
+                    foreach (var permission in site.Permissions)
                     {
-                        GetSiteRootWeb(site).RoleAssignments.Remove(GetSiteRootWeb(rootSite).SiteGroups[permission.GroupName]);
+                        logger.Debug(string.Format(
+                            "Deleting permission level: {0} from group: {1} for site: {2}", 
+                            permission.PermissionLevelId, 
+                            permission.GroupName, 
+                            site.Name));
+                        GetSiteRootWeb(siteToDelete).RoleAssignments.Remove(GetSiteRootWeb(rootSite).SiteGroups[permission.GroupName]);
+                        logger.Debug(string.Format("Done. Deleting permission level: {0} from group: {1} for site: {2}", 
+                            permission.PermissionLevelId, 
+                            permission.GroupName, 
+                            site.Name));
                     }
 
-                    sites.Delete(collection.Name);
+                    logger.Debug(string.Format("Done. Deleting permissions for site {0}", site.Name));
+                    logger.Debug(string.Format("Deleting site: {0} itself", site.Name));
+                    sites.Delete(site.Name);
+                    logger.Debug(string.Format("Done. Deleting site {0}", site.Name));
                 }
-            }
+                else
+                {
+                    logger.Debug(string.Format("Can't locate site: {0}. Skipped", site.Name));
+                }
 
+                logger.Debug(string.Format("Done. Processing site {0}", site.Name));
+            }
+            logger.Debug("Done. Deleting sites.");
+
+            logger.Debug("Deleting groups");
             foreach (var group in configuration.Groups)
             {
+                logger.Debug(string.Format("Deleteng group: {0}", group.Name));
                 GetSiteRootWeb(rootSite).SiteGroups.Remove(group.Name);
+                logger.Debug(string.Format("Done. Deleting group: {0}", group.Name));
             }
+            logger.Debug("Done. Deleting groups");
+
+            logger.Debug(string.Format("Done. Feature {0} deactivation", properties.Feature.Definition.DisplayName));
         }
 
-        private static SitesConfiguration GetSitesConfiguration(SPFeatureReceiverProperties properties, IConfigPropertiesParser parser)
+        private static SitesConfiguration GetSitesConfiguration(
+            SPFeatureReceiverProperties properties, 
+            IConfigPropertiesParser parser)
         {
             return parser.ParseSitesConfiguration(properties.Feature.Properties["SitesConfiguration"].Value);
         }
@@ -126,7 +211,48 @@ namespace FLS.SharePoint.SiteStructure.Features.CreateSitesCollection
             return ((SPWebApplication) properties.Feature.Parent).Sites[0];
         }
 
-        private static SPSiteCollection GetSiteCollection(SPFeatureReceiverProperties properties)
+        private void ApplyWebTemplate(SPWeb web, string templateName)
+        {
+            SPWebTemplateCollection templates = web.GetAvailableWebTemplates(GetLocale(web));
+            var template = (from SPWebTemplate t in templates
+                            where t.Title == templateName
+                            select t).FirstOrDefault();
+
+            if (template != null)
+            {
+                try
+                {
+                    web.ApplyWebTemplate(template);
+                }
+                catch (SPException exception)
+                {
+                    logger.Debug(exception.NativeErrorMessage);
+                }
+            }
+        }
+
+        private void LoadTemplateFromPackage(SPSite site, string packageFileName, string packageDirectory)
+        {
+            SPDocumentLibrary solutions = (SPDocumentLibrary)site.GetCatalog(SPListTemplateType.SolutionCatalog);
+            SPFile solutionFile = solutions.RootFolder.Files.Add(
+                packageFileName,
+                fileSystemHelper.ReadFile(
+                    fileSystemHelper.CombinePath(packageDirectory, packageFileName)));
+
+            SPUserSolution solution = site.Solutions.Add(solutionFile.Item.ID);
+            Guid solutionId = solution.SolutionId;
+            SPFeatureDefinitionCollection siteFeatures = site.FeatureDefinitions;
+            var features = from SPFeatureDefinition f
+                               in siteFeatures
+                           where f.SolutionId.Equals(solutionId) && f.Scope == SPFeatureScope.Site
+                           select f;
+            foreach (SPFeatureDefinition feature in features)
+            {
+                site.Features.Add(feature.Id, false, SPFeatureDefinitionScope.Site);
+            }
+        }
+
+        private static SPSiteCollection GetSiteCollections(SPFeatureReceiverProperties properties)
         {
             return ((SPWebApplication)properties.Feature.Parent).Sites;
         }
@@ -134,6 +260,11 @@ namespace FLS.SharePoint.SiteStructure.Features.CreateSitesCollection
         private static SPWeb GetSiteRootWeb(SPSite rootSite)
         {
             return rootSite.RootWeb;
+        }
+
+        private static uint GetLocale(SPWeb web)
+        {
+            return (uint)web.Locale.LCID;
         }
 
         private static bool SiteCollectionExists(SPSiteCollection sites, string siteName)
