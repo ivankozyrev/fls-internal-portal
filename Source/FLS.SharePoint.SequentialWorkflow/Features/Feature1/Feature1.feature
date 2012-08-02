a020ddac5a6451ec6673fbc8c775114cd4c2c087
vanyaisaev <ivan.isaev@firstlinesoftware.com>
2012-08-03 02:47:21 +0400

    added solution with workflow example

---
+++ Source/FLS.SharePoint.SequentialWorkflow/Features/Feature1/Feature1.feature
@@ -0,0 +1,12 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="950f2b5f-4ab3-4d3e-9362-4888b465a48c" featureId="950f2b5f-4ab3-4d3e-9362-4888b465a48c" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="FLS.SharePoint.SequentialWorkflow Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="dc526fda-22d0-4ae0-9f4f-f5480561e906" />
+    <projectItemReference itemId="90a1a537-c0ca-41d7-8453-ca28b1041408" />
+    <projectItemReference itemId="203b6f76-7a15-48c5-bd19-942fa0212ecc" />
+    <projectItemReference itemId="b043a086-8650-4f8f-bfc5-b89dae082459" />
+    <projectItemReference itemId="c33b7b69-e952-4f8e-83a7-2307637910d4" />
+    <projectItemReference itemId="e29e742a-e963-47d8-af6f-1a78712ac65c" />
+    <projectItemReference itemId="33e62daa-33dc-4d06-8bd3-74fdf4a05952" />
+  </projectItems>
+</feature>
---
+++ Source/FLS.SharePoint.SequentialWorkflow/WorkflowForms/TaskForm.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.WebControls;
+using Microsoft.SharePoint.WebPartPages;
+
+namespace FLS.SharePoint.SequentialWorkflow.WorkflowForms
+{
+    public partial class TaskForm : WebPartPage
+    {
+        protected override void OnPreInit(EventArgs e)
+        {
+            base.OnPreInit(e);
+            this.MasterPageFile = SPContext.Current.Web.MasterUrl;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            btnSaveAsDraft.Click += OnBtnSaveAsDraftClick;
+            btnComplete.Click += OnBtnCompleteClick;
+            btnCancel.Click += OnBtnCancelClick;
+        }
+
+        private void OnBtnCancelClick(object sender, EventArgs e)
+        {
+            CloseForm();
+        }
+
+        private void CloseForm()
+        {
+            if ((SPContext.Current != null) && SPContext.Current.IsPopUI)
+            {
+                this.Context.Response.Write("<script type='text/javascript'>window.frameElement.commitPopup();</script>");
+                this.Context.Response.Flush();
+                this.Context.Response.End();
+            }
+            else
+            {
+                string str = this.Page.Request.QueryString["Source"];
+                if (!string.IsNullOrEmpty(str))
+                {
+                    SPUtility.Redirect(string.Empty, SPRedirectFlags.UseSource, this.Context);
+                }
+            }
+        }
+
+        private void OnBtnCompleteClick(object sender, EventArgs e)
+        {
+            var li = SPContext.Current.ListItem;
+            li[SPBuiltInFieldId.TaskStatus] = "Tasks_Completed";
+            li[SPBuiltInFieldId.PercentComplete] = 1;
+
+            SaveButton.SaveItem(SPContext.Current, false, string.Empty);
+
+            CloseForm();
+        }
+
+        private void OnBtnSaveAsDraftClick(object sender, EventArgs e)
+        {
+            SaveButton.SaveItem(SPContext.Current, false, string.Empty);
+
+            CloseForm();
+        }
+    }
+}
