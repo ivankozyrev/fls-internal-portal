5524113a8bf2c32fa3d91688358e3822836c254d
Evgeny Skurikhin <eskurihin@gmail.com>
2012-08-07 08:46:54 +0400

    registering services on all sites

--- Source/FLS.SharePoint.System/Features/ServiceLocator/ServiceLocator.EventReceiver.cs
+++ Source/FLS.SharePoint.System/Features/ServiceLocator/ServiceLocator.EventReceiver.cs
@@ -2,6 +2,7 @@ using System.Runtime.InteropServices;
 using FLS.SharePoint.Utils;
 using Microsoft.Practices.SharePoint.Common.ServiceLocation;
 using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
 
 namespace FLS.SharePoint.System.Features.ServiceLocator
 {
@@ -13,14 +14,43 @@ namespace FLS.SharePoint.System.Features.ServiceLocator
             var serviceLocator = SharePointServiceLocator.GetCurrent();
             var typeMappings = serviceLocator.GetInstance<IServiceLocatorConfig>();
 
-            typeMappings.RegisterTypeMapping<IConfigPropertiesParser, ConfigPropertiesParser>();
-            typeMappings.RegisterTypeMapping<IFileSystemHelper, FileSystemHelper>();
+            // TODO: dirty approach. Should use farm scope and full trust proxy to access mappings from sandboxed code
+            var applications = SPWebService.ContentService.WebApplications;
+            foreach (var application in applications)
+            {
+                foreach (SPSite site in application.Sites)
+                {
+                    RegisterMapping(typeMappings, site);
+                }
+            }
         }
 
         public override void FeatureDeactivating(SPFeatureReceiverProperties properties)
         {
             var serviceLocator = SharePointServiceLocator.GetCurrent();
             var typeMappings = serviceLocator.GetInstance<IServiceLocatorConfig>();
+
+            // TODO: dirty approach. Should use farm scope and full trust proxy to access mappings from sandboxed code
+            var applications = SPWebService.ContentService.WebApplications;
+            foreach (var application in applications)
+            {
+                foreach (SPSite site in application.Sites)
+                {
+                    UnregisterMapping(typeMappings, site);
+                }
+            }
+        }
+
+        private static void RegisterMapping(IServiceLocatorConfig typeMappings, SPSite site)
+        {
+            typeMappings.Site = site;
+            typeMappings.RegisterTypeMapping<IConfigPropertiesParser, ConfigPropertiesParser>();
+            typeMappings.RegisterTypeMapping<IFileSystemHelper, FileSystemHelper>();
+        }
+
+        private static void UnregisterMapping(IServiceLocatorConfig typeMappings, SPSite site)
+        {
+            typeMappings.Site = site;
             typeMappings.RemoveTypeMapping<IConfigPropertiesParser>(null);
             typeMappings.RemoveTypeMapping<IFileSystemHelper>(null);
         }
