aac946f5726b8d20da10161b50e810bc6ab50846
oprikhodko <olga.prikhodko@firstlinesoftware.com>
2012-08-20 13:42:35 +0400

    Added new project ContentDeployment for creating migration job.

---
+++ Source/FLS.SharePoint.ContentDeployment/ExportContentJob.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.SharePoint.Administration;
+
+namespace FLS.SharePoint.DocumentApproval
+{
+    public class ExportContentJob : SPJobDefinition
+    {
+
+    }
+}
---
+++ Source/FLS.SharePoint.ContentDeployment/ImportContentJob.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.SharePoint.Administration;
+using Microsoft.SharePoint.Deployment;
+
+namespace FLS.SharePoint.DocumentApproval
+{
+    public class ImportContentJob : SPJobDefinition
+    {
+        public ImportContentJob() 
+               : base()
+        {
+        }
+
+        public override void Execute(System.Guid targetInstanceId)
+        {
+            var settings = new SPExportSettings();
+            settings.SiteUrl = "http://localhost/sites/authoring";
+            settings.ExportMethod = SPExportMethodType.ExportAll;
+            settings.FileLocation = @"c:\export";
+            settings.FileCompression = false;
+            settings.CommandLineVerbose = true;
+
+            var export = new SPExport(settings);
+            export.Run(); 
+        }
+    }
+}
